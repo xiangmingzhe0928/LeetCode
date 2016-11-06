26c97725c0012fe5cf8e9352e9f80d6fa16a9467
XiangMZ <402062338@qq.com>
2016-11-06 22:31:40 +0800

    listnode update

--- src/LinkedList/ListNode.java
+++ src/LinkedList/ListNode.java
@@ -23,4 +23,23 @@ public class ListNode {
     public String toString() {
         return "value:"+this.val+"--->next:"+this.next;
     }
+
+    /**
+     * 提供初始链表数据：以ListNode(1)为head
+     * @param n 初始链表个数 要求n>=1
+     * @return
+     */
+    public static ListNode getListNodeData(int n){
+        if(n < 1)
+            return null;
+        ListNode head = new ListNode(1);
+        ListNode temp = head;
+        for (int i = 2;i<=n ;i++){
+           head.next = new ListNode(i);
+           head = head.next;
+        }
+
+        return  temp;
+    }
+
 }
--- src/LinkedList/RemoveNodeFromEnd_19.java
+++ src/LinkedList/RemoveNodeFromEnd_19.java
@@ -43,12 +43,13 @@ public class RemoveNodeFromEnd_19 {
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next = new ListNode(6);
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(4);
+//        head.next.next.next.next = new ListNode(5);
+//        head.next.next.next.next.next = new ListNode(6);
+        ListNode head = ListNode.getListNodeData(5);
 
         System.out.println(removeNthFromEnd(head,2));
 
