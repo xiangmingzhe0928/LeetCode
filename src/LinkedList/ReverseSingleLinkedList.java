d917647b93d9b00418c907a009f59da9ff846911
xiang mingzhe <xiangmz0928@gmail.com>
2018-01-29 10:46:42 +0800

    reverse single list

---
+++ src/LinkedList/ReverseSingleLinkedList.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+/**
+ * LinkedList
+ * description:反转单链表
+ * 一个经典的老题目了 今天同事讨论起相关的问题 再写下加深下吧
+ * author:mingzhe Xiang
+ * date:2018/1/29
+ */
+public class ReverseSingleLinkedList {
+
+	private static ListNode reverse(ListNode head) {
+		if (null == head || null == head.next) {
+			return head;
+		}
+
+		ListNode prevNode = null;
+		ListNode currNode = head;
+		ListNode nextNode = head.next;
+
+		while (currNode.next != null) {
+			currNode.next = prevNode;
+			prevNode = currNode;
+			currNode = nextNode;
+			nextNode = currNode.next;
+		}
+		currNode.next = prevNode;
+		return currNode;
+	}
+
+	public static void main(String[] args) {
+		ListNode head = ListNode.getListNodeData(5);
+		System.out.println("before:" + head);
+		System.out.println("after:" + reverse(head));
+	}
+}
